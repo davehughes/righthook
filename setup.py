6c4934e5245a8aee8a84f899d32f65b482805f47
David Hughes <d@vidhughes.com>
2014-07-25 12:43:56 -0700

    Preparation for PyPi

--- setup.py
+++ setup.py
@@ -6,9 +6,16 @@ setuptools.setup(
     name='righthook',
     version='0.0.1',
     packages = setuptools.find_packages(exclude=[]),
+    author = 'David Hughes',
+    author_email = 'd@vidhughes.com',
+    url = 'https://github.com/davehughes/righthook',
+    download_url = 'https://github.com/davehughes/righthook/tarball/0.0.1',
+    keywords = [
+        'Django', 'Celery', 'task', 'queue', 'webhook',
+        ],
     install_requires = [
         'django-celery>=3.0',
-        ]
+        ],
     )
 
 
